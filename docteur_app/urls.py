355ca4e4a08afe3c0ae7a0ed5a620dafdfda89ea
EloM28 <mugisheloge@gmail.com>
2025-02-23 23:38:30 +0200

    Added other urls

--- docteur_app/urls.py
+++ docteur_app/urls.py
@@ -1,17 +1,23 @@
 from .import views
 from django.urls import path
-from .views import UserRegistrationView, LoginView, DashboardView, LogoutView
+from .views import UserRegistrationView, LoginView, DashboardView, LogoutView, PredictView, PredictAPIView, LoginAPIView
 
 
 
 
 urlpatterns = [
-    path("home/",views.home,name="home"),
+    # path("",DashboardView.as_view(), name="home"),
     
     path("register/", UserRegistrationView.as_view(), name='register'),
     path("login/", LoginView.as_view(), name='login'),
     path("logout/", LogoutView.as_view(), name='logout'),
     path("dashboard/",DashboardView.as_view(), name="dashboard"),
-    path("home/",views.register,name="home"),
-    path("dash/",views.dashboard,name="dash")
+    path("api/predict/",PredictAPIView.as_view(),name='api_predict'),
+    path("predict/", PredictView.as_view(), name='predict'),
+    
+    
+    # API Routes
+    
+    path("api/login/", LoginAPIView.as_view(), name='api_login'),
+    
 ]
